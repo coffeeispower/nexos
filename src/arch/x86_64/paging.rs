96a29e784c97c37f1a71795886b92ba32091489a
coffee-is-power <tiagodinis33@proton.me>
2023-11-23 09:56:32 +0000

    feat: add PageEntry struct

--- src/arch/x86_64/paging.rs
+++ src/arch/x86_64/paging.rs
@@ -1,3 +1,29 @@
+use bitflags::bitflags;
+
+bitflags! {
+    #[repr(C)]
+    pub struct PageEntry: usize {
+        /// Indicates if the CPU should map this page or not
+        /// If the CPU tries to access this page witht this bit cleared, it will throw a Page Fault
+        const PRESENT = 1 << 0;
+        /// Allow this page to be written to
+        const WRITABLE = 1 << 1;
+        /// Only allow the kernel to access this page
+        const KERNEL_ONLY = 1 << 2;
+        const WRITE_THROUGH = 1 << 3;
+        /// Disables CPU cache
+        const DISABLE_CACHE = 1 << 4;
+        /// This bit is set automatically by the CPU whenever this page gets accessed
+        const ACCESSED = 1 << 5;
+        const LARGER_PAGES = 1 << 7;
+        const _ = !0;
+    }
+}
+impl PageEntry {
+    pub fn phys_addr(&self) -> usize {
+        (self.bits() & 0xFFFFFFFFFFFFF000) >> (4 * 3)
+    }
+}
 /// Calculates the correct indices in the page table for a given virtual memory address
 /// For example if you want to map 0x1300-0x2300 to 0x4000-5000, you would need to put the address 0x1300 into the following indices: 3 - 0 - 0 - 0
 /// So this function calculates those indices in the page table
